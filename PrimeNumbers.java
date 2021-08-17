9a351a9a461155a589b5b30e688733c1723cc6d4
vismayatk002 <vismayatk002@gmail.com>
2021-08-17 22:23:11 +0530

    Refactor find prime numbers at the range 0 to 1000

---
+++ PrimeNumbers.java
@@ -0,0 +1,27 @@
+public class PrimeNumbers{
+
+    int min = 0;
+    int max = 1000;
+    public void isPrime(){
+
+        int flag = 1;
+        System.out.println("Prime numbers ");
+        for(int i=min; i<=max; i++){
+            flag = 1;
+            for(int j=2; j<=(i/2); j++){
+                
+                if(i % j == 0){
+                    flag = 0;
+                }
+            }
+            if(flag == 1){
+                System.out.println(i);
+            }
+        }
+    }
+    public static void main(String[] args){
+
+        PrimeNumbers prime = new PrimeNumbers();
+        prime.isPrime();
+    }
+}
